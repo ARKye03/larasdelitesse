1bba78ed0b9ed75cb94312ea7e2c3022f078d922
ARKye03 <rafa03-dev@proton.me>
2025-11-24 19:13:38 -0300

    feat: Introduce product resource including model, migration, controller, and web routes.

---
+++ app/Models/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image_name',
+        'og_image_name',
+    ];
+}
