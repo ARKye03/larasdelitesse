2143189ffc163f7beb1c315f1c8863f6c23cced3
ARKye03 <rafa03-dev@proton.me>
2025-12-03 09:53:58 -0300

    feat: reposition and restyle the create product link, and update its href

--- resources/js/pages/products.tsx
+++ resources/js/pages/products.tsx
@@ -15,13 +15,15 @@ export default function Products() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Products" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                <Link
-                    as="button"
-                    href={route('products.create')}
-                    className="flex items-center gap-2"
-                >
-                    <CirclePlus />
-                </Link>
+                <div className="ml-auto">
+                    <Link
+                        as="button"
+                        href={'products/create'}
+                        className="rounded-sm p-2 transition-colors duration-200 hover:bg-gray-800"
+                    >
+                        <CirclePlus />
+                    </Link>
+                </div>
                 <table className="overflow-hidden rounded-2xl">
                     <thead>
                         <tr className="bg-sky-900 text-white">
